0b0a02b4211b750384f535378a1c82ce6355f9bf
Mateusz Czernik <Mateusz.Czernik@microsoft.wsei.edu.pl>
2023-09-29 17:01:32 +0200

    Tokeny

---
+++ PhotoProjectAPI/Dataset/VM/UserManagment/RequestForTokenViewmodel.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotoProjectAPI.Dataset.VM.UserManagment
+{
+    public class RequestForTokenViewmodel
+    {
+        public string Token { get; set; }
+        public string TokenRefreshment { get; set; }
+
+    }
+}
---
+++ PhotoProjectAPI/Dataset/VM/UserManagment/UserLoginViewmodel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhotoProjectAPI.Dataset.VM.UserManagment
+{
+    public class UserLoginViewmodel
+    {
+        [Required(ErrorMessage = "Please provide a login")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Please provide a password")]
+        public string Password { get; set; }
+    }
+}

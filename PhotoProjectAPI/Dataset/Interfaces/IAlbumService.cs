c793e50a4e57d98036f8a6364e950afe5b85661e
Mateusz Czernik <Mateusz.Czernik@microsoft.wsei.edu.pl>
2023-10-01 18:48:20 +0200

    Tokeny, użytkownicy, JWT, przygotowanie interfejsu dla albumu

---
+++ PhotoProjectAPI/Dataset/Interfaces/IAlbumService.cs
@@ -0,0 +1,40 @@
+﻿using PhotoProjectAPI.Dataset.VM;
+using PhotoProjectAPI.DTO;
+using PhotoProjectAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhotoProjectAPI.Data.Interfaces
+{
+    public interface IAlbumService
+    {
+        public void CreateAlbum(AlbumViewmodel album, string userId, bool isAdmin);
+        public List<AlbumDTO> GetAllAlbums();
+        public List<PhotoDTO> GetAllPhotosFromAlbum(int albumId);
+
+        public AlbumDTO GetAlbumUsingId(int albumId);
+        public Album GetAlbumUsingIdPriv(int albumId);
+        public List<AlbumDTO> GetAlbumsUsingName(string albumName);
+        public List<AlbumDTO> GetAlbumsUsingAuthorName(string authorName);
+        public List<AlbumDTO> GetAlbumsUsingAuthorId(string authorId);
+
+        public string GetUserIdUsingAlbumId(int albumId);
+        public PhotoAlbum GetAlbumPhotoUsingIds(int albumId, int photoId);
+
+        public bool HasAlbumAccess(int albumId, string userId, bool isAdmin);
+        public bool HasAlbumPrivileges(int albumId, string userId, bool isAdmin);
+        public bool HasAlbumPrivilegesWithDestAlbumId(int albumId, int destAlbumId, string userId, bool isAdmin);
+        public bool HasPhotoPrivileges(int photoId, string userId, bool isAdmin);
+
+        public bool AddPhotoToAlbumUsingIds(bool isAdmin, string currentUserId, int albumId, List<int> photoIds);
+        public bool MovePhotosToAlbum(bool isAdmin, string currentUserId, int currentAlbumId, int destinationAlbumId, List<int> photoIds);
+        public bool RemovePhotoFromAlbumUsingId(bool isAdmin, string currentUserId, int albumId, List<int> photoIds);
+
+        public bool DoesAlbumExist(int albumId);
+        public string ChangeAlbumAccessUsingId(int albumId);
+        public string ChangeAlbumAccessForAllUsingId(int albumId);
+        public AlbumDTO UpdateAlbum(int albumId, [FromForm] AlbumUpdateViewmodel album, string userId);
+       
+        public void DeleteAlbumUsingId(int albumId);      
+        public string DeleteAlbumAndPhotos(int albumId);
+    }
+}
--- PhotoProjectAPI/Program.cs
+++ PhotoProjectAPI/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PhotoProjectAPI.Data;
+using PhotoProjectAPI.Models;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,10 +23,72 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 //TUTAJ USLUGI DODAC
 
+//Identity
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+
+//Validation parametrs for tokens
+var tokenValidationParameters = new TokenValidationParameters()
+{
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["JWT:Secret"])),
+
+    ValidateIssuer = true,
+    ValidIssuer = builder.Configuration["JWT:Issuer"],
+
+    ValidateAudience = true,
+    ValidAudience = builder.Configuration["JWT:Audience"],
+
+    ValidateLifetime = true,
+    ClockSkew = TimeSpan.Zero,
+};
+builder.Services.AddSingleton(tokenValidationParameters);
+
+//JWT Bearer and Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = tokenValidationParameters;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "Bearer Authentication with JWT Token",
+        Type = SecuritySchemeType.Http,
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Id = "Bearer",
+                Type = ReferenceType.SecurityScheme
+            }
+        },
+        new List<string>()
+    }
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
